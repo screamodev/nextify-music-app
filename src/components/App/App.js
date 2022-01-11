de67a4c9a7b524e653b711af7a18f7cb4caca5a6
Arsenii Yurchenko <arsenii.iurchenko@gmail.com>
2022-01-11 15:49:54 +0200

    Add app component

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import Footer from "../Footer";
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <section className="main-container" />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
