75573666de36721b2290d5889be43f38f243854f
Arsenii Yurchenko <arsenii.iurchenko@gmail.com>
2022-01-12 19:04:19 +0200

    Provide logic to player component

--- src/components/Footer/Player/Player.js
+++ src/components/Footer/Player/Player.js
@@ -1,15 +1,22 @@
+import { useContext } from "react";
 import { AiFillPauseCircle, AiFillPlayCircle } from "react-icons/ai";
+import { PlayerContext } from "../../../contexts/PlayerContext";
 import "./player.scss";
 
 function Player() {
+  const { isPlaying, togglePlay } = useContext(PlayerContext);
+
   return (
     <div className="player">
       <div className="player-controls">
         <div className="player-controls-buttons">
           <div className="player-controls-play-button">
-            <button>
-              <AiFillPlayCircle className="play-toggle-button-icon" />
-              <AiFillPauseCircle className="play-toggle-button-icon" />
+            <button onClick={togglePlay}>
+              {isPlaying ? (
+                <AiFillPauseCircle className="play-toggle-button-icon" />
+              ) : (
+                <AiFillPlayCircle className="play-toggle-button-icon" />
+              )}
             </button>
           </div>
         </div>
