dacfd191bc41d5a99e7b3ca07d75d25157f2f4d8
Arsenii Yurchenko <arsenii.iurchenko@gmail.com>
2022-01-14 15:59:37 +0200

    Create progress bar logic

--- src/components/Footer/Player/Player.js
+++ src/components/Footer/Player/Player.js
@@ -1,16 +1,24 @@
 import { useContext } from "react";
 import { AiFillPauseCircle, AiFillPlayCircle } from "react-icons/ai";
 import { PlayerContext } from "../../../contexts/PlayerContext";
+import { formatTime } from "../../../helpers/audio";
 import "./player.scss";
 
 function Player() {
-  const { isPlaying, togglePlay } = useContext(PlayerContext);
+  const {
+    isPlaying,
+    progress,
+    current,
+    audioDuration,
+    togglePlay,
+    changeCurrentTime,
+  } = useContext(PlayerContext);
 
   return (
     <div className="player">
       <div className="player-controls">
-        <div className="player-controls-buttons">
-          <div className="player-controls-play-button">
+        <div className="controls-buttons">
+          <div className="controls-play-button">
             <button onClick={togglePlay}>
               {isPlaying ? (
                 <AiFillPauseCircle className="play-toggle-button-icon" />
@@ -19,6 +27,18 @@ function Player() {
               )}
             </button>
           </div>
+        </div>
+        <div className="controls-progress-bar-holder">
+          <span>{formatTime(current)}</span>
+          <input
+            className="controls-progress-bar"
+            type="range"
+            value={progress}
+            max="100"
+            step="1"
+            onChange={(event) => changeCurrentTime(event.target.value)}
+          />
+          <span>{formatTime(audioDuration)}</span>
         </div>
       </div>
     </div>
--- src/contexts/PlayerContext.js
+++ src/contexts/PlayerContext.js
@@ -1,28 +1,36 @@
-import { createContext, useRef, useState } from "react";
+import { createContext } from "react";
 import { useSelector } from "react-redux";
+import { usePlayer } from "../hooks/usePlayer";
 
-export const PlayerContext = createContext({});
+export const PlayerContext = createContext({
+  isPlaying: false,
+  progress: 0,
+  current: 0,
+  audioDuration: 0,
+  togglePlay: () => {},
+  changeCurrentTime: () => {},
+});
 
 export function PlayerProvider({ children }) {
-  const [isPlaying, setIsPlaying] = useState(false);
   const { url } = useSelector(({ player }) => player.songs[0]);
-  const player = useRef(new Audio(url));
-
-  const togglePlay = () => {
-    setIsPlaying((prevState) => !prevState);
-
-    if (isPlaying) {
-      player.current.pause();
-    } else {
-      player.current.play();
-    }
-  };
+  const {
+    isPlaying,
+    progress,
+    current,
+    audioDuration,
+    togglePlay,
+    changeCurrentTime,
+  } = usePlayer(url);
 
   return (
     <PlayerContext.Provider
       value={{
         isPlaying,
+        progress,
+        current,
+        audioDuration,
         togglePlay,
+        changeCurrentTime,
       }}
     >
       {children}
