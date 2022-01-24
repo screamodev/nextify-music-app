b40f6bdc6294671c13f703c79874bd4b113c3ee5
Arsenii Yurchenko <arsenii.iurchenko@gmail.com>
2022-01-24 15:21:07 +0200

    Add name props to input component

--- src/components/common/FormInput/FormInput.js
+++ src/components/common/FormInput/FormInput.js
@@ -1,13 +1,19 @@
 import "./formInput.scss";
 
-function FormInput(props) {
-  const { label, type = "text", placeholder, value, onChange } = props;
-
+function FormInput({
+  label,
+  name,
+  placeholder,
+  value,
+  onChange,
+  type = "text",
+}) {
   return (
     <div className="form-input-block">
       <label>{label}</label>
       <input
         className="form-input"
+        name={name}
         type={type}
         value={value}
         placeholder={placeholder}
