06babe2961ec8567c945e634827390f5bdb2969c
Arsenii Yurchenko <arsenii.iurchenko@gmail.com>
2022-01-25 16:44:54 +0200

    Add interceptors

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,3 +3,13 @@ import * as axios from "axios";
 export const instance = axios.create({
   baseURL: process.env.REACT_APP_BASE_API_URL,
 });
+
+instance.interceptors.request.use((request) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    request.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return request;
+});
