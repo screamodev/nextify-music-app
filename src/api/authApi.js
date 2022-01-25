c97f27ae374341f9627ef7906591b6f96f6fc2a5
Arsenii Yurchenko <arsenii.iurchenko@gmail.com>
2022-01-25 16:45:20 +0200

    Add sign in api request

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,3 +1,5 @@
 import { instance } from "./axiosInstance";
 
 export const signUp = (userData) => instance.post("signup", userData);
+
+export const signIn = (userData) => instance.post("signin", userData);
