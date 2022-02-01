06c6c8cc5d1068e15d41e32f219d7a32540cffbb
Arsenii Yurchenko <arsenii.iurchenko@gmail.com>
2022-02-01 18:57:56 +0200

    Add sidebar component

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,3 +1,11 @@
 export const AUTH_PAGE = "/authorization";
 
 export const MAIN_PAGE = "/";
+
+export const PROFILE = "/profile";
+
+export const SEARCH = "/search";
+
+export const CREATE_PLAYLIST = "/create-playlist";
+
+export const FAVORITE = "/favorite";
