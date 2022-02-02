39d2216b79611a87e6ef0d548c8fe0f088731efc
Arsenii Yurchenko <arsenii.iurchenko@gmail.com>
2022-02-02 13:04:27 +0200

    Add menu bubble animation

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,39 +1,42 @@
 import { NavLink } from "react-router-dom";
-import "./sidebar.scss";
+import { bubble as Menu } from "react-burger-menu";
 import {
   CREATE_PLAYLIST,
   FAVORITE,
   PROFILE,
   SEARCH,
 } from "../../constants/routes";
+import "./sidebar.scss";
 
 function Sidebar() {
   return (
-    <aside className="aside">
-      <nav className="nav-menu">
-        <ul className="nav-list">
-          <li className="nav-item">
-            <NavLink to={PROFILE}>Profile</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to={SEARCH}>Search</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to={CREATE_PLAYLIST}>Create playlist</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to={FAVORITE}>Favorite songs</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <hr className="dividing-line" />
-      <div className="playlists">
-        <ul className="playlists-list">
-          <li>Reggae</li>
-          <li>Rock</li>
-          <li>Pop</li>
-        </ul>
-      </div>
+    <aside>
+      <Menu className="aside-menu">
+        <nav className="nav-menu">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <NavLink to={PROFILE}>Profile</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to={SEARCH}>Search</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to={CREATE_PLAYLIST}>Create playlist</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to={FAVORITE}>Favorite songs</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <hr className="dividing-line" />
+        <div className="playlists">
+          <ul className="playlists-list">
+            <li>Reggae</li>
+            <li>Rock</li>
+            <li>Pop</li>
+          </ul>
+        </div>
+      </Menu>
     </aside>
   );
 }
