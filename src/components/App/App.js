673b0750310319e6a03bf5b07d4ea0d713feb982
Arsenii Yurchenko <arsenii.iurchenko@gmail.com>
2022-02-03 15:57:03 +0200

    Add search component to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,10 +4,11 @@ import { Routes, Route } from "react-router-dom";
 import { PlayerProvider } from "../../contexts/PlayerContext";
 import AuthorizationPage from "../../pages/AuthorizationPage";
 import MyProfilePage from "../../pages/MyProfilePage";
+import SearchPage from "../../pages/SearchPage";
 import GuestRoute from "../AuthRoutes/GuestRoute";
 import ProtectedRoute from "../AuthRoutes/ProtectedRoute";
 import store from "../../store";
-import { AUTH_PAGE, MAIN_PAGE } from "../../constants/routes";
+import { AUTH_PAGE, MAIN_PAGE, SEARCH } from "../../constants/routes";
 import "./app.scss";
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
               </Route>
               <Route element={<ProtectedRoute />}>
                 <Route path={MAIN_PAGE} element={<MyProfilePage />} />
+                <Route path={SEARCH} element={<SearchPage />} />
               </Route>
             </Routes>
           </div>
