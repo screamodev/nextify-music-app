5b0fd038569703ac5b55e45989d6331e60ddc503
Arsenii Yurchenko <arsenii.iurchenko@gmail.com>
2022-02-16 11:21:21 +0200

    Add user playlist view

--- src/api/playlistApi.js
+++ src/api/playlistApi.js
@@ -2,3 +2,6 @@ import { instance } from "./axiosInstance";
 
 export const createPlaylist = (playlist) =>
   instance.post("playlists", playlist);
+
+export const getUserPlaylists = (userId) =>
+  instance.get(`users/${userId}?_embed=playlists`);
--- src/store/playlistsSlice.js
+++ src/store/playlistsSlice.js
@@ -1,14 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getUserPlaylists } from "../api/playlistApi";
+
+export const getPlaylists = createAsyncThunk(
+  "playlists/getPlaylists",
+  async (userId) => {
+    const { data } = await getUserPlaylists(userId);
+
+    return data;
+  }
+);
 
 const initialState = {
-  name: "",
-  description: "",
-  songs: [],
+  playlists: [],
+};
+
+const onFulfilled = (state, { payload: { playlists } }) => {
+  state.playlists = playlists.reverse();
 };
 
 const playlistsSlice = createSlice({
   name: "playlists",
   initialState,
+  extraReducers: {
+    [getPlaylists.fulfilled]: onFulfilled,
+  },
 });
 
 export default playlistsSlice.reducer;
