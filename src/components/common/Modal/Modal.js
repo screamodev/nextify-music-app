1b920f95a23f823963eae2cad0e6c77c416d2c1e
Arseniy Yurchenko <arsenii.iurchenko@gmail.com>
2022-03-11 19:29:15 +0200

    Add animation to modal component

--- src/components/common/Modal/Modal.js
+++ src/components/common/Modal/Modal.js
@@ -1,13 +1,64 @@
 import PropTypes from "prop-types";
+import { motion, AnimatePresence } from "framer-motion";
 import "./modal.scss";
 
 function Modal({ isOpen, children }) {
+  const initialOverlay = {
+    opacity: 0,
+  };
+
+  const overlayAnimation = {
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+    },
+  };
+
+  const overlayExit = {
+    opacity: 0,
+    transition: {
+      delay: 0.3,
+    },
+  };
+
+  const initialModal = {
+    scale: 0,
+  };
+
+  const modalAnimation = {
+    scale: 1,
+    transition: {
+      duration: 0.3,
+    },
+  };
+
+  const modalExit = {
+    scale: 0,
+    transition: {
+      delay: 0.3,
+    },
+  };
+
   return (
-    isOpen && (
-      <div className="registration-modal-overlay">
-        <div className="modal-window">{children}</div>
-      </div>
-    )
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          initial={initialOverlay}
+          animate={overlayAnimation}
+          exit={overlayExit}
+          className="modal-overlay"
+        >
+          <motion.div
+            initial={initialModal}
+            animate={modalAnimation}
+            exit={modalExit}
+            className="modal-window"
+          >
+            {children}
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
 
