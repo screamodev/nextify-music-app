bef8887f36c38a8c76f4ff9c7fdb9975e78ef483
Arseniy Yurchenko <arsenii.iurchenko@gmail.com>
2022-03-28 14:04:43 +0300

    Fix music overlay bug

--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -5,7 +5,6 @@ import { getPlaylists } from "../../store/playlistsSlice";
 import { getFavorites } from "../../store/favoriteSongsSlice";
 import Nav from "../Header";
 import Sidebar from "../Sidebar";
-import Footer from "../Footer";
 import "./mainLayout.scss";
 
 function MainLayout({ children }) {
@@ -18,16 +17,13 @@ function MainLayout({ children }) {
   }, [userId]);
 
   return (
-    <>
-      <div className="wrapper-main">
-        <Sidebar />
-        <div className="main-content">
-          <Nav />
-          {children}
-        </div>
+    <div className="wrapper-main">
+      <Sidebar />
+      <div className="main-content">
+        <Nav />
+        {children}
       </div>
-      <Footer />
-    </>
+    </div>
   );
 }
 
