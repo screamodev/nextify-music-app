a11faf891e1ad3ba3a3b5417ac3447e3912710c7
Arseniy Yurchenko <yurchenkoa@cp3238-lx>
2022-04-14 22:13:19 +0300

    Add queue pop-up to player

--- src/components/Footer/Player/Player.js
+++ src/components/Footer/Player/Player.js
@@ -1,14 +1,23 @@
-import { useContext } from "react";
-import { AiFillPauseCircle, AiFillPlayCircle } from "react-icons/ai";
+import { useContext, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  AiFillCheckCircle,
+  AiFillPauseCircle,
+  AiFillPlayCircle,
+} from "react-icons/ai";
 import { BiSkipPrevious, BiSkipNext } from "react-icons/bi";
 import {
   BsFillVolumeDownFill,
   BsFillVolumeMuteFill,
   BsFillVolumeUpFill,
 } from "react-icons/bs";
+import { IoMdHeart } from "react-icons/io";
+import { MdQueueMusic } from "react-icons/md";
 import { PlayerContext } from "../../../contexts/PlayerContext";
+import { updateFavorites } from "../../../store/favoriteSongsSlice";
 import { formatTime } from "../../../helpers/audio";
 import { useAudio } from "../../../hooks/useAudio";
+import Queue from "./Queue";
 import "./player.scss";
 
 function Player() {
@@ -34,17 +43,50 @@ function Player() {
     toggleMute,
   } = useAudio(currentSong, isPlaying, onPause);
 
-  const { name, author } = currentSong;
+  const userId = useSelector((state) => state.auth.user?.id);
+  const favoriteSongsIds = useSelector(
+    (state) => state.favorites.favoriteSongsIds
+  );
+  const dispatch = useDispatch();
+  const { name, author, id } = currentSong;
+  const isFavorite = favoriteSongsIds.includes(id);
+  const [isQueueShown, setIsQueueShown] = useState(false);
+
+  const switchQueue = () => {
+    setIsQueueShown(!isQueueShown);
+  };
+
+  const closeQueue = () => {
+    setIsQueueShown(false);
+  };
+
+  const makeFavorite = (songId) => {
+    dispatch(
+      updateFavorites({
+        userId,
+        favoriteSongsIds: [...favoriteSongsIds, songId],
+      })
+    );
+  };
+
+  const unmakeFavorite = (songId) => {
+    const updatedFavoriteSongsIds = favoriteSongsIds.filter(
+      (trackId) => trackId !== songId
+    );
+    dispatch(
+      updateFavorites({ userId, favoriteSongsIds: updatedFavoriteSongsIds })
+    );
+  };
 
   const getVolumeIcon = () => {
     if (volume > 0.5 && !isMuted) {
-      return <BsFillVolumeUpFill className="volume-toggle-button-icon" />;
+      return <BsFillVolumeUpFill className="volume-button-icon" />;
     }
     if (volume <= 0.5 && !isMuted && volume !== 0) {
-      return <BsFillVolumeDownFill className="volume-toggle-button-icon" />;
+      return <BsFillVolumeDownFill className="volume-button-icon" />;
     }
     if (volume === 0 || isMuted) {
-      return <BsFillVolumeMuteFill className="volume-toggle-button-icon" />;
+      return <BsFillVolumeMuteFill className="volume-button-icon" />;
     }
     return null;
   };
@@ -96,19 +138,40 @@ function Player() {
           <span>{formatTime(audioDuration)}</span>
         </div>
       </div>
-      <div className="volume-controls">
-        <button className="volume-button" onClick={toggleMute}>
-          {getVolumeIcon()}
+      <div className="side-buttons">
+        <button className="side-buttons-button" onClick={switchQueue}>
+          <MdQueueMusic className="side-buttons-button-icon" />
         </button>
-        <input
-          className="volume-control"
-          type="range"
-          value={isMuted ? "0" : volume}
-          max="1"
-          step="0.05"
-          onChange={(e) => changeVolume(e.target.value)}
-        />
+        {isFavorite ? (
+          <button
+            onClick={() => unmakeFavorite(id)}
+            className="side-buttons-button"
+          >
+            <AiFillCheckCircle className="side-buttons-button-icon" />
+          </button>
+        ) : (
+          <button
+            onClick={() => makeFavorite(id)}
+            className="side-buttons-button"
+          >
+            <IoMdHeart className="side-buttons-button-icon" />
+          </button>
+        )}
+        <div className="side-buttons-volume">
+          <button className="side-buttons-button" onClick={toggleMute}>
+            {getVolumeIcon()}
+          </button>
+          <input
+            className="side-buttons-volume-control"
+            type="range"
+            value={isMuted ? "0" : volume}
+            max="1"
+            step="0.05"
+            onChange={(e) => changeVolume(e.target.value)}
+          />
+        </div>
       </div>
+      {isQueueShown && <Queue closeQueue={closeQueue} />}
     </div>
   );
 }
